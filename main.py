165a6ca4d83107e45146585ba31f1725c1e954e4
mkae21 <bbuchu123@gmail.com>
2024-11-10 21:46:49 +0900

    Add : SOS
    SOS 기능 구현.
    1. 차량 속력 0으로 만들기
    2. 엔진 정지
    3. 왼쪽, 오른쪽 문 열기
    4. 트렁크 열기

--- main.py
+++ main.py
@@ -22,11 +22,29 @@ def execute_command_callback(command, car_controller):
         car_controller.lock_left_door() # 왼쪽문 잠금
     elif command == "LEFT_DOOR_UNLOCK":
         car_controller.unlock_left_door() # 왼쪽문 잠금해제
+    elif command == "RIGHT_DOOR_LOCK":
+        car_controller.lock_right_door() # 오른쪽문 잠금
+    elif command == "RIGHT_DOOR_UNLOCK":
+        car_controller.unlock_right_door() # 오른쪽문 잠금해제
     elif command == "LEFT_DOOR_OPEN":
         car_controller.open_left_door() # 왼쪽문 열기
     elif command == "LEFT_DOOR_CLOSE":
         car_controller.close_left_door() # 왼쪽문 닫기
+    elif command == "RIGHT_DOOR_OPEN":
+        car_controller.open_right_door() # 오른쪽문 열기
+    elif command == "RIGHT_DOOR_CLOSE":
+        car_controller.close_right_door() # 오른쪽문 닫기
     elif command == "TRUNK_OPEN":
+        car_controller.open_trunk() # 트렁크 열기
+    elif command == "SOS": 
+        while car_controller.get_speed() == 0: # 차량 속력이 0이 될 때까지
+            car_controller.brake()
+
+        if car_controller.get_engine_status() != True: 
+            car_controller.toggle_engine() # 엔진 정지
+
+        car_controller.unlock_left_door() # 왼쪽문 열기
+        car_controller.unlock_right_door() #오른쪽문 열기
         car_controller.open_trunk() # 트렁크 열기
 
 
